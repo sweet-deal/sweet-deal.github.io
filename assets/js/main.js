79bef3704222c7aa23d2508711f8380d44d397d3
sweet-deal <101293499+sweet-deal@users.noreply.github.com>
2022-03-10 07:37:15 +0100

    izbacila author

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,10 +7,6 @@ window.onload = function(){
             "href" : "index.html"
         },
         {
-            "naziv" : "About Us",
-            "href" : "about-us.html"
-        },
-        {
             "naziv" : "Products",
             "href" : "products.html"
         },
