99983640e5c0d6ae5c276531e2238dad806b7c54
firedupcandles <95667387+firedupcandles@users.noreply.github.com>
2022-03-10 23:02:09 +0100

    ajax json

---
+++ assets/js/main2.js
@@ -0,0 +1,35 @@
+window.onload = function(){
+    $.ajax({
+        url: "assets/data/navMeni.json",
+        method: "get",
+        dataType: "json",
+        success: function(data){
+            console.log(data);
+            ispisListiLinkova(data);
+            ispisListiLinkovaFuter(data);
+        },
+        error: function(xhr){
+            console.log(xhr);
+        }
+    })
+
+    
+}
+function ispisListiLinkova (podaci){
+    let html = `<ul>`;
+    for (let li of podaci){
+        html += `<a href="${li.href}"><li>${li.naziv}</li></a>`;
+    }
+    html += `</ul>`;
+
+    document.querySelector("#nav-meni").innerHTML = html;
+}
+function ispisListiLinkovaFuter (podaci){
+    let html = `<ul>`;
+    for (let li of podaci){
+        html += `<a href="${li.href}"><li>${li.naziv}</li></a>`;
+    }
+    html += `</ul>`;
+
+    document.querySelector("#nav-meni-futer").innerHTML = html;
+}
