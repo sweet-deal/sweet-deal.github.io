cafb81c86a230c131d0b6cc6261fa43403303f55
firedupcandles <95667387+firedupcandles@users.noreply.github.com>
2022-03-10 07:11:27 +0100

    index tekst

--- assets/js/main.js
+++ assets/js/main.js
@@ -250,26 +250,26 @@ window.onload = function(){
         {
             "id" : 1,
             "href" : "assets/images/hearts.jpg",
-            "naziv" : "Card Title",
-            "tekst" : "Text"
+            "naziv" : "Gift for a loved one",
+            "tekst" : "Our specially made and decorated products are a perfect way to brighten someones day or to make it even sweeter! Show someone you care with our custom candy"
         },
         {
             "id" : 2,
             "href" : "assets/images/cupcakes.jpg",
-            "naziv" : "Card Title",
-            "tekst" : "Text"
+            "naziv" : "Eco-friendly and eco-aware",
+            "tekst" : "From every purchase you make we donate 5% to various organizations in our joint effort to elevate our ecological status"
         },
         {
             "id" : 3,
             "href" : "assets/images/gummies.jpg",
-            "naziv" : "Card Title",
-            "tekst" : "Text"
+            "naziv" : "Vegan and gluten-free options",
+            "tekst" : "Take a pick at some of our specially prepared vegan and gluten-free sweets! We use only the best, organic products"
         },
         {
             "id" : 4,
             "href" : "assets/images/macarons.jpg",
-            "naziv" : "Card Title",
-            "tekst" : "Text"
+            "naziv" : "Delivery, free of charge",
+            "tekst" : "For every purchase over 20$ or containing more than 5 products, we will deliver the goods to your doorstep for free!"
         }
     ]
 
